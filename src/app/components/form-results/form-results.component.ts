687029ae868d0ef2965633d2bc6c1e279a41ae25
kisar18 <martinpur99@seznam.cz>
2022-05-05 16:17:49 +0200

    made result variable in form-results private

--- src/app/components/form-results/form-results.component.ts
+++ src/app/components/form-results/form-results.component.ts
@@ -8,7 +8,7 @@ import { MobileMenuService } from 'src/app/services/mobile-menu.service';
 })
 export class FormResultsComponent implements OnInit {
   
-  result;
+  private result;
 
   constructor(private mobileMenuService: MobileMenuService) {
   }
@@ -16,6 +16,14 @@ export class FormResultsComponent implements OnInit {
   ngOnInit() {
     this.result = history.state;
   }
+
+  getFirstName() { return this.result.firstName; }
+  getLastName() { return this.result.lastName; }
+  getEmail() { return this.result.email; }
+  getGender() { return this.result.gender; }
+  getFavouriteColor() { return this.result.favouriteColor; }
+  getEmployed() { return this.result.employed; }
+  getNotes() { return this.result.notes; }
 
   closeMobileMenu() {
     this.mobileMenuService.emitChange(false);
